ae6331eaaed21a2f39e346bcfb2f2b5197fc71c9
JosonYang <258490116@qq.com>
2023-04-03 11:27:22 +0800

    feat: add mathjax to mermaid

--- mermaid/src/index.js
+++ mermaid/src/index.js
@@ -3,26 +3,48 @@ const micro = require('micro')
 const { Worker, SyntaxError } = require('./worker')
 const Task = require('./task')
 const instance = require('./browser-instance')
+const mathjax = require('mathjax-node')
 
 ;(async () => {
+
+  mathjax.config({
+    MathJax: {
+      
+    }
+  })
+  mathjax.start()
   // QUESTION: should we create a pool of Chrome instances ?
   const browser = await instance.create()
   logger.info(`Chrome accepting connections on endpoint ${browser.wsEndpoint()}`)
   const worker = new Worker(browser)
   const server = micro(async (req, res) => {
-    // TODO: add a /_status route (return mermaid version)
-    // TODO: read the diagram source as plain text
     const url = new URL(req.url, 'http://localhost') // create a URL object. The base is not important here
     const outputType = url.pathname.match(/\/(png|svg)$/)?.[1]
-    const isStatus = url.pathname.match(/\/(_status)$/) !== null
-    if (outputType) {
+    const isStatus = url.pathname.indexOf('_status') > -1
+    const isLatex = url.pathname.indexOf('latex') > -1
+    if (outputType && !isStatus) {
       const diagramSource = await micro.text(req, { limit: '1mb', encoding: 'utf8' })
       if (diagramSource) {
         try {
           const isPng = outputType === 'png'
-          const output = await worker.convert(new Task(diagramSource, isPng), url.searchParams)
-          res.setHeader('Content-Type', isPng ? 'image/png' : 'image/svg+xml')
-          return micro.send(res, 200, output)
+          if(isLatex) {
+            res.setHeader('Content-Type', 'image/svg+xml')
+            mathjax.typeset({
+                math: diagramSource,
+                format: "TeX",  // or "inline-TeX", "MathML"
+                svg: true,      // or svg:true, or html:true
+            }).then((data)=>{
+              if (!data.errors) {
+                return micro.send(res, 200, data.svg)
+              }
+            })
+            return;
+            // return micro.send(res, 500, 'An error occurred while converting the diagram')
+          } else {
+            const output = await worker.convert(new Task(diagramSource, isPng), url.searchParams)
+            res.setHeader('Content-Type', isPng ? 'image/png' : 'image/svg+xml')
+            return micro.send(res, 200, output)
+          }
         } catch (err) {
           if (err instanceof SyntaxError) {
             return micro.send(res, 400, err.message)
